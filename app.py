98d7e7dafb86dcef8e5dfd4a04bf9e1f0eaf2748
李欣 <JustinDellAdam@live.com>
2020-01-06 17:06:35 +0800

    make shell context

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 from flask_migrate import Migrate
 
 from app_core import create_app, db
+from app_core.models import User, Role
 
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 if os.path.exists(dotenv_path):
@@ -12,6 +13,11 @@ if os.path.exists(dotenv_path):
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 migrate = Migrate(app, db)
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return dict(db=db, User=User, Role=Role)
 
 
 @app.cli.command()

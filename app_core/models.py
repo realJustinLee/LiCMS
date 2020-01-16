225a715d1a64773587f9a5952bf5cff7a1f064fe
李欣 <JustinDellAdam@live.com>
2020-01-16 10:46:33 +0800

    Add User Gravatar
    Added:
    - User Gravatar

--- app_core/models.py
+++ app_core/models.py
@@ -1,3 +1,4 @@
+import hashlib
 from datetime import datetime
 
 from flask import current_app
@@ -98,15 +99,16 @@ class Role(db.Model):
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128))
     email = db.Column(db.String(128), unique=True, index=True)
-    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    avatar_hash = db.Column(db.String(32))
     password_hash = db.Column(db.String(128))
     confirmed = db.Column(db.Boolean, default=False)
-    name = db.Column(db.String(128))
     location = db.Column(db.String(128))
     about_me = db.Column(db.Text())
     member_since = db.Column(db.DateTime(), default=datetime.utcnow)
     last_seen = db.Column(db.DateTime(), default=datetime.utcnow)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     gender_id = db.Column(db.Integer, db.ForeignKey('genders.id'))
 
     def __init__(self, **kwargs):
@@ -116,6 +118,8 @@ class User(UserMixin, db.Model):
                 self.role = Role.query.filter_by(name='Administrator').first()
             if self.role is None:
                 self.role = Role.query.filter_by(default=True).first()
+        if self.email is not None and self.avatar_hash is None:
+            self.avatar_hash = self.gravatar_hash()
 
     @property
     def password(self):
@@ -180,7 +184,7 @@ class User(UserMixin, db.Model):
         if self.query.filter_by(email=new_email).first() is not None:
             return False
         self.email = new_email
-        # self.avatar_hash = self.gravatar_hash()
+        self.avatar_hash = self.gravatar_hash()
         db.session.add(self)
         return True
 
@@ -193,6 +197,15 @@ class User(UserMixin, db.Model):
     def ping(self):
         self.last_seen = datetime.utcnow()
         db.session.add(self)
+
+    def gravatar_hash(self):
+        return hashlib.md5(self.email.lower().encode('utf-8')).hexdigest()
+
+    def gravatar(self, size=100, default='identicon', rating='g'):
+        url = 'https://secure.gravatar.com/avatar'
+        _hash = self.avatar_hash or self.gravatar_hash()
+        return '{url}/{hash}?s={size}&d={default}&r={rating}'.format(
+            url=url, hash=_hash, size=size, default=default, rating=rating)
 
     def __repr__(self):
         return '<User %r>' % self.username

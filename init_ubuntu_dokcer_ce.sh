dcb8cff5b71ec186c2c9e0e618799cb68745f2bf
李欣 <JustinDellAdam@live.com>
2020-03-20 22:37:43 +0800

    Update init_ubuntu_dokcer_ce.sh

--- init_ubuntu_dokcer_ce.sh
+++ init_ubuntu_dokcer_ce.sh
@@ -15,7 +15,7 @@ apt-get install \
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 apt-key fingerprint 0EBFCD88
 
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $codename stable"
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
 # Update the `apt` package index:
 apt-get update -y && apt-get upgrade -y && apt-get dist-upgrade -y

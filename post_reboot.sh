7f7434f36d0087471827eb99ecbd2e5d25f6c782
Justin Lee <JustinDellAdam@live.com>
2022-08-12 13:21:53 +0800

    using docker compose v2

--- post_reboot.sh
+++ post_reboot.sh
@@ -1,9 +1,9 @@
 # Invoke app via docker-compose
 echo 'Initializing docker-compose'
-docker-compose build --no-cache
-docker-compose down
-docker-compose pull
-docker-compose up -d >> docker-licms.log
+docker compose build --no-cache
+docker compose down
+docker compose pull
+docker compose up -d >> docker-licms.log
 
 echo 'Auto cleaning...'
 docker system prune -a

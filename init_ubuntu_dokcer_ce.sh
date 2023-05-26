368620e22ab35993e7265312f3fe38903119dfd0
Justin Lee <JustinDellAdam@live.com>
2023-05-26 20:03:32 +0800

    Update init_ubuntu_dokcer_ce.sh

--- init_ubuntu_dokcer_ce.sh
+++ init_ubuntu_dokcer_ce.sh
@@ -2,22 +2,20 @@
 
 apt-get update -y && apt-get upgrade -y && apt-get dist-upgrade -y
 
-apt-get install \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release
+apt-get install ca-certificates curl gnupg -y
 
-mkdir -p /etc/apt/keyrings
+install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
 
 echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 apt-get update -y && apt-get upgrade -y && apt-get dist-upgrade -y
 
-apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
 apt-get install python3 python3-pip unzip -y
 

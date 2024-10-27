8373e75108bd8068d9f1da51542b50b1e9b2d070
Justin Lee <JustinDellAdam@live.com>
2024-10-27 18:36:09 +0800

    Update init_ubuntu_dokcer_ce.sh

--- init_ubuntu_dokcer_ce.sh
+++ init_ubuntu_dokcer_ce.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 
 # Add Docker's official GPG key:
-apt-get update -y && apt-get upgrade -y && apt-get dist-upgrade -y
-apt-get install ca-certificates curl gnupg -y
+apt update -y && apt full-upgrade -y
+apt install ca-certificates curl
 install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 chmod a+r /etc/apt/keyrings/docker.asc
@@ -13,10 +13,9 @@ echo \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-apt-get update -y && apt-get upgrade -y && apt-get dist-upgrade -y
+apt update -y && apt full-upgrade -y
+apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
-apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
-
-apt-get install python3 python3-pip unzip -y
+apt install python3 python3-pip unzip -y
 
 return

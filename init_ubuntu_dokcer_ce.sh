7f34301a283364178825129820a6c8df4355fc78
Justin Lee <JustinDellAdam@live.com>
2025-07-27 20:26:54 +0800

    Update init_ubuntu_dokcer_ce.sh

--- init_ubuntu_dokcer_ce.sh
+++ init_ubuntu_dokcer_ce.sh
@@ -10,7 +10,7 @@ chmod a+r /etc/apt/keyrings/docker.asc
 # Add the repository to Apt sources:
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 apt update -y && apt full-upgrade -y

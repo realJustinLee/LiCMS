39a547c6e65708df7be2e49bc968ea96a0895667
Justin Lee <JustinDellAdam@live.com>
2026-05-21 11:12:34 +0800

    Update init_ubuntu_dokcer_ce.sh

--- init_ubuntu_dokcer_ce.sh
+++ init_ubuntu_dokcer_ce.sh
@@ -8,11 +8,12 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/doc
 chmod a+r /etc/apt/keyrings/docker.asc
 
 # Add the repository to Apt sources:
-sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+tee /etc/apt/sources.list.d/docker.sources <<EOF
 Types: deb
 URIs: https://download.docker.com/linux/ubuntu
 Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
 Components: stable
+Architectures: $(dpkg --print-architecture)
 Signed-By: /etc/apt/keyrings/docker.asc
 EOF
 
